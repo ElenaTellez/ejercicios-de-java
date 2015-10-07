cadc558a969577bdcc435f5db4c1b873728247fa
ElenaTellez <metellezs@hotmail.com>
2015-10-07 23:30:52 +0200

    ejercicios tema 4

---
+++ LECCION 4/Ejercicio1104.java
@@ -0,0 +1,27 @@
+/**
+ * Ejercicio 11 Leccion 4
+ *
+ * Escribe un programa que dada una hora determinada (horas y minutos), calcule los
+ * segundos que faltan para llegar a la medianoche.
+ *
+ * @author Elena Téllez
+ */
+public class Ejercicio1104 {
+
+	public static void main(String[] args) {
+
+	System.out.println("Vamos a calcular los segundos que faltan para llegar a medianoche."); 
+    System.out.println("Por favor, introduzca la hora que es sin minutos: ");    
+	int hora = Integer.parseInt(System.console().readLine());	
+	
+	System.out.println("Por favor, introduzca los minutos: ");
+	int minuto = Integer.parseInt(System.console().readLine());
+	
+	int segundosHora = ((hora * 87600)/24);
+	int segundosMinuto = ((minuto * 3600)/60);
+	int respuesta = (87600 - (segundosHora + segundosMinuto));
+	
+	System.out.println("Faltan " + respuesta + " segundos para llegar a medianoche");
+					
+	}
+}
---
+++ LECCION 4/Ejercicio1204.java
@@ -0,0 +1,30 @@
+
+
+/**
+ * Ejercicio 12 Leccion 4
+ *
+ * Realiza un minicuestionario con 10 preguntas tipo test sobre las asignaturas que se imparten
+ * en el curso. Cada pregunta acertada sumará un punto. El programa mostrará al final la
+ * calificación obtenida. Pásale el minicuestionario a tus compañeros y pídeles que lo hagan
+ * para ver qué tal andan de conocimientos en las diferentes asignaturas del curso.
+ *
+ * @author Elena Téllez
+ */
+public class Ejercicio1204 {
+
+	public static void main(String[] args) {
+
+    System.out.println("A continuación vamos a realizar un test de las diferentes asignaturas del curso.");    
+	System.out.println("Por favor, introduzca verdadero o falso según sea la respuesta correcta. ");
+	System.out.println("¿Se llama Luis el profesor de la asignatura de Programacion?");
+	int a = Integer.parseInt(System.console().readLine());
+	
+	int a = (verdadero);
+		
+	if (a == verdadero) {
+			System.out.println("Incorrecta");
+			} else {
+			System.out.println("Correcto");
+			}				
+	}
+}
