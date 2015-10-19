aaa142ba664bcbca5f9fed9c68698a8bd535ca2a
ElenaTellez <metellezs@hotmail.com>
2015-10-19 13:19:18 +0200

    leccion 5

---
+++ LECCION 5/Ejercicio0705.java
@@ -0,0 +1,44 @@
+/**
+ * Ejercicio 7 Lección 5
+ * 
+ * Realiza el control de acceso a una caja fuerte. La combinación
+ * será un número de 4 cifras. El programa nos pedirá la combinación
+ * para abrirla. Si no acertamos, se nos mostrará el mensaje
+ * “Lo siento, esa no es la combinación” y si acertamos se nos dirá
+ * “La caja fuerte se ha abierto satisfactoriamente”. Tendremos cuatro
+ * oportunidades para abrir la caja fuerte.
+ * 
+ * @author Elena Téllez
+ */
+public class Ejercicio0705 {
+
+  public static void main(String[] args) {
+    
+    int intentos = 4;
+    int numeroIntroducido;
+    boolean correcto = false;
+    
+    do {
+      System.out.print("Introduzca la combinación de la caja fuerte: ");
+      int combinacion = Integer.parseInt(System.console().readLine());
+      
+      if (combinacion == 1979) {
+        correcto = true;
+      } else {
+        System.out.println("Combinación incorrecta");
+      }
+      
+      intentos--; //intentos = intentos - 1;
+  
+    } while((intentos > 0) && (!correcto));
+    
+    if (correcto) {
+      System.out.println("La combinación es correcta.");
+    } else {
+      System.out.println("Su caja fuerte está bloqueada.");
+    }
+  }
+}
+
+//boolean: se utiliza para las variables con dos significados: verdadero o falso, true o false, yes or no, 1 or 0. 
+// estructura: boolean variable = true 
---
+++ LECCION 5/Ejercicio0805.java
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio 8 Lección 5
+ * 
+ * Muestra la tabla de multiplicar de un número introducido por teclado.
+ * 
+ * @author Elena Téllez
+ */
+public class Ejercicio0805 {
+
+  public static void main(String[] args) {
+    
+    int numero;
+      
+    System.out.print("¡Vamos a aprender las tablas de multiplicar!\n");
+         
+    System.out.print("Por favor introduzca un número del 1 al 10: ");
+    numero = Integer.parseInt(System.console().readLine());
+
+    for (int i = 1; i < 11; i++) {         
+      System.out.println(numero + "x" + i + " = " + (numero * i));
+    }                 
+  }
+}
