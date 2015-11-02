b71e522e1d7cc72c198cbbd8e78509907cac9328
ElenaTellez <metellezs@hotmail.com>
2015-11-02 10:59:55 +0100

    practicando

--- LECCION 6/AleatorioTemperatura.java
+++ LECCION 6/AleatorioTemperatura.java
@@ -1,18 +1,57 @@
 /**
-* Generación de números aleatorios.
+* Escribe un programa que genere el menú del día de un restaurante vegetariano. El programa debe pedir el día
+* de la semana, que será un número del uno al siete, y a continuación debe mostrar de forma aleatoria la
+* secuencia de platos en función de las siguientes condiciones:
+* (a) De primer plato puede haber ensalada, pasta o arroz
+* (b) De segundo plato puede haber hamburguesas de soja, filetes de seitán o albóndigas de tofu 
+* que irán  acompañados con una guarnición que puede ser patatas bravas, 
+* verduras a la plancha o pimientos asados 
+* (c) De postre puede haber flan, helado o arroz con leche 
+* (d) Cuando el primer plato es arroz, el postre no puede ser arroz con leche 
+* (e) Los sábados y domingos no hay menú (se debe mostrar un mensaje que así lo indique).
+* Ejemplo:
+* Selecciona un día de la semana (un número del 1 al 7): 5
+* Menú del día: pasta de primero, albóndigas de soja con patatas bravas de segundo y arroz con leche de postre.
 *
-* @author Luis José Sánchez
+* @author Elena Téllez
 */
 
-public class AleatorioTemperatura {
+public class Examen20142015preg1 {
   public static void main(String[] args) {
 
-    System.out.println("La previsión del tiempo para la próxima semana:");
+    System.out.println("Por favor, elija un día de la semana (1 al 7):");
+		int dia= Integer.parseInt(System.console().readLine());
     
-    for (int i = 1; i <= 7; i++) {
-      System.out.print(((int)(Math.random()*16) + 15 ) + "ºC "); //el intervalo entre 15 y 30 al sumar 15
-    }
-
-    System.out.println();
+    
+    String menu = "";
+    
+	
+       
+			    
+      switch ((Math.random()*7)){ //genera el carácter de relleno
+          
+          
+          case 1:
+            dia = "pasta";
+            break;  
+          
+          case 2:
+            dia = "arroz";
+            break;
+          
+          case 3:
+            dia = "ensalada";
+            break;
+          
+                    
+            default:
+        
+        }          
+			
+    
+      
+			System.out.println(); //es necesario para dar orden a los bucles. 
+			
+			
   }
 }
