2cc587e3ec20707f310abe886d98ee5a411bce3a
ElenaTellez <metellezs@hotmail.com>
2015-11-20 13:05:09 +0100

    leccion 7

---
+++ LECCION 7/Ejer0707.java
@@ -0,0 +1,42 @@
+/**
+ * Ejercicio 7
+ *
+ * Escribe un programa que genere 100 números aleatorios del 0 al 20 y que los muestre
+por pantalla separados por espacios. El programa pedirá entonces por teclado dos valores
+y a continuación cambiará todas las ocurrencias del primer valor por el segundo en
+la lista generada anteriormente. Los números que se han cambiado deben aparecer
+entrecomillados.
+ * 
+ * @author Elena Téllez
+ */
+
+public class Ejer0707{
+  public static void main(String[] args) {
+    
+    int[]numero = new int[100];    
+    
+    for (int i = 0; i < 100; i++) {
+      
+      numero[i] = (int)(Math.random()*20);            
+      
+      System.out.print(" " + numero[i]);
+      
+    }
+    
+    System.out.println("\nPor favor, introduzca un numero del 1 al 20:");  
+    int numUno = Integer.parseInt(System.console().readLine()); 
+    
+    System.out.println("\nPor favor, introduzca otro numero:");  
+    int numDos = Integer.parseInt(System.console().readLine()); 
+    
+    for (int i = 0; i < 100; i++) {
+      
+      if (numero[i] == numUno) {
+        
+        System.out.print("'" + numDos + "'");
+      } else {
+          System.out.printf(" " + numero[i]);
+        }  
+    }
+  }
+}
---
+++ LECCION 7/Ejer0807.java
@@ -0,0 +1,55 @@
+/**
+ * Ejercicio 8
+ *
+ * Realiza un programa que pida la temperatura media que ha hecho en cada mes de un
+determinado año y que muestre a continuación un diagrama de barras horizontales con
+esos datos. Las barras del diagrama se pueden dibujar a base de asteriscos o cualquier otro
+carácter.
+ * 
+ * @author Elena Téllez
+ */
+
+public class Ejer0807{
+  public static void main(String[] args) {
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de enero:");  
+    int enero = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de febrero:");  
+    int febrero = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de marzo:");  
+    int marzo = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de abril:");  
+    int abril = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de mayo:");  
+    int mayo = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de junio:");  
+    int junio = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de julio:");  
+    int julio = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de agosto:");  
+    int agosto = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de septiembre:");  
+    int septiembre = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de octubre:");  
+    int octubre = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de noviembre:");  
+    int noviembre = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Por favor, introduzca la temperatura media que ha hecho en el mes de diciembre:");  
+    int diciembre = Integer.parseInt(System.console().readLine());
+    
+    
+    
+    
+  }
+}
